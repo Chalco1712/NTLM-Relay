385785f691dd981aed025978df5d7c6b5cd668f3
root <root@localhost.localdomain>
2020-02-02 06:46:55 +0000

    ICMP MAYBE WORKING

--- nfscript.py
+++ nfscript.py
@@ -4,31 +4,32 @@ import os
 from scapy.all import *
 
 #iptablesr = "iptables -A FORWARD -i eth0 -j ACCEPT"
-#iptablesr2 = "iptables -t nat -A PREROUTING -i eth0 -p icmp -j NFQUEUE --queue-num 1"
-#iptablesr = "iptables -I INPUT -p tcp --dport 445 -j NFQUEUE --queue-num 1"
-iptablesr2 = "iptables -I INPUT -i eth0 -j NFQUEUE --queue-num 1"
+iptablesr2 = "iptables -t nat -A PREROUTING -i eth0 -p icmp -j DNAT --to 10.10.10.89"
+iptablesr = "iptables -I INPUT -p icmp -j NFQUEUE --queue-num 1"
+iptablesr3 = "iptables -t nat -A POSTROUTING -i eth0 -s 10.10.10.3 -p icmp -j DNAT --to 10.10.10.4"
+iptablesr3 = "iptables -t nat -A POSTROUTING -i eth0 -s 10.10.10.4 -p icmp -j DNAT --to 10.10.10.3"
+#iptablesr2 = "iptables -I INPUT -i eth0 -j NFQUEUE --queue-num 1"
 #iptablesr2 = "iptables -A OUTPUT -j NFQUEUE --queue-num 1"
 #iptablesr = "iptables -t nat -A PREROUTING -i eth0 -p icmp -j NFQUEUE --queue-num 1"
 #iptablesr2 = "iptables -"
 
 print("Adding iptable rules")
-#print(iptablesr)
+print(iptablesr)
 print(iptablesr2)
-#os.system(iptablesr)
+os.system(iptablesr)
 os.system(iptablesr2)
 
 def callback(packet):
     data = packet.get_payload()
     pkt = IP(data)
     print(pkt.show())
-    packet.drop()
-    #packet.set_payload(raw(pkt))
-    #packet.accept()
+    #packet.drop()
+    #packet.set_payload(raw(pk))
+    packet.accept()
 
 def main():
     q = NetfilterQueue()
     q.bind(1, callback)
-    #q.set_callback(callback)
     s = socket.fromfd(q.get_fd(), socket.AF_INET, socket.SOCK_STREAM)
     try:
         q.run_socket(s)
