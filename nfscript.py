13a4a2954b77cf70c8c26f3891fd9a76e4605365
root <root@localhost.localdomain>
2020-02-02 07:59:17 +0000

    new stuff

--- nfscript.py
+++ nfscript.py
@@ -4,10 +4,16 @@ import os
 from scapy.all import *
 
 #iptablesr = "iptables -A FORWARD -i eth0 -j ACCEPT"
-iptablesr2 = "iptables -t nat -A PREROUTING -i eth0 -p icmp -j DNAT --to 10.10.10.89"
-iptablesr = "iptables -I INPUT -p icmp -j NFQUEUE --queue-num 1"
-iptablesr3 = "iptables -t nat -A POSTROUTING -i eth0 -s 10.10.10.3 -p icmp -j DNAT --to 10.10.10.4"
-iptablesr3 = "iptables -t nat -A POSTROUTING -i eth0 -s 10.10.10.4 -p icmp -j DNAT --to 10.10.10.3"
+chains = "iptables -N client-incoming"
+chains2 = "iptables -N dc-incoming"
+
+client = "iptables -A client-incoming -t nat -A PREROUTING -i eth0 -p tcp --dport 445 -j DNAT --to 10.10.10.89"
+client1 = "iptables -A client-incoming -I INPUT -p tcp --dport 445 -j NFQUEUE --queue-num 1"
+client2 = "iptables -A client-incoming -t nat -A POSTROUTING -o eth0 -s 10.10.10.89 -p tcp --dport 445 -j SNAT --to 10.10.10.4"
+iptablesr2 = "iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 445 -j DNAT --to 10.10.10.89"
+iptablesr = "iptables -I INPUT -p tcp --dport 445 -j NFQUEUE --queue-num 1"
+iptablesr3 = "iptables -t nat -A POSTROUTING -o eth0 -s 10.10.10.3 -p tcp --dport 445 -j SNAT --to 10.10.10.4"
+iptablesr4 = "iptables -t nat -A POSTROUTING -o eth0 -s 10.10.10.4 -p tcp -j SNAT --to 10.10.10.3"
 #iptablesr2 = "iptables -I INPUT -i eth0 -j NFQUEUE --queue-num 1"
 #iptablesr2 = "iptables -A OUTPUT -j NFQUEUE --queue-num 1"
 #iptablesr = "iptables -t nat -A PREROUTING -i eth0 -p icmp -j NFQUEUE --queue-num 1"
@@ -18,6 +24,8 @@ print(iptablesr)
 print(iptablesr2)
 os.system(iptablesr)
 os.system(iptablesr2)
+os.system(iptablesr3)
+os.system(iptablesr4)
 
 def callback(packet):
     data = packet.get_payload()
